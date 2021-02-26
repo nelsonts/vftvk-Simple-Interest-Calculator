1fa1f2e84016b0bfd6b146b362ab1e754121b887
Tom Nelson <nelsonts@us.ibm.com>
2021-02-26 15:27:46 +0000

    step 6, exercise 4 - part 2

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 // and calculates/displays the results
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+    var principal = document.getElementById("principal");
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100; 
 
-    var result = document.getElementById("result");
-    result.innerHTML = "If you deposit " + principal + "," 
-        + " at an interest rate of " + rate + "%."
-        + " You will receive an amount of " + interest + ","
-        + " in the year " + calcYear(years);
+    if (principal.value <= 0) {
+        alert("Enter a positive number");
+        principal.focus();
+    } else {
+
+        var interest = principal.value * years * rate / 100; 
+
+        var result = document.getElementById("result");
+        result.innerHTML = "If you deposit " + principal.value + "," 
+            + " at an interest rate of " + rate + "%."
+            + " You will receive an amount of " + interest + ","
+            + " in the year " + calcYear(years);
+    }
 
 }
 
@@ -20,5 +27,12 @@ function compute()
 function calcYear(year_offset) {
     var currentYear = new Date().getFullYear;
     return currentYear + year_offset;
+}
+
+function ratechange()
+{
+    var rate = document.getElementById("rate").value;
+    var ratevalue = document.getElementById("ratevalue");
+    ratevalue.innerHTML = rate + "%";
 }
         
